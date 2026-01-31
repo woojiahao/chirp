c7fe5bb7ba83ceb63f123d2b63a5805358463f60
Jiahao, Woo <woojiahao1234@gmail.com>
2026-01-31 18:46:50 +0800

    Add receiver for rom path

--- src/main.c
+++ src/main.c
@@ -1,12 +1,21 @@
 #include "chirp.h"
+#include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+  if (argc < 2)
+  {
+    printf("usage: %s <rom path>\n", argv[0]);
+    return 1;
+  }
+
   printf("welcome to chirp!\n");
   printf("starting chirp...\n");
+  const char *path = argv[1];
+  printf("loading ROM at path %s\n", path);
 
   // TODO: Add stats for how long the emulator is running for
-  Chirp *chirp = chirp_new();
+  Chirp *chirp = chirp_new(path);
   chirp_start_emulator_loop(chirp);
 
   return 0;
