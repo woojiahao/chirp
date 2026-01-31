fa66a1b21054fa20e9e960c68ed118d9581b1ec6
Jiahao, Woo <woojiahao1234@gmail.com>
2026-01-31 13:37:14 +0800

    Add base chirp entrypoint

---
+++ chirp.c
@@ -0,0 +1,12 @@
+//
+// Created by Woo Jia Hao on 31/1/26.
+//
+
+#include "chirp.h"
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world\n");
+    return 0;
+}
---
+++ chirp.h
@@ -0,0 +1,8 @@
+//
+// Created by Woo Jia Hao on 31/1/26.
+//
+
+#ifndef CHIRP_CHIRP_H
+#define CHIRP_CHIRP_H
+
+#endif //CHIRP_CHIRP_H
