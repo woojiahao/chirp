72c9513e8f2abc155aaa68a0fe0a12218f897c03
Jiahao, Woo <woojiahao1234@gmail.com>
2026-02-03 19:53:59 +0800

    Add full instruction set

--- src/instructions.h
+++ src/instructions.h
@@ -4,12 +4,41 @@
 #include "chirp_t.h"
 
 // all instruction names found here: https://tobiasvl.github.io/blog/write-a-chip-8-emulator/#instructions
-void clear_screen(Chirp *chirp);
-void jump(Chirp *chirp);
-void set_register_x(Chirp *chirp);
-void add_value_to_register_x(Chirp *chirp);
-void set_index_register(Chirp *chirp);
-void display(Chirp *chirp);
-void return_from_subroutine(Chirp *chirp);
+// notes on instructions can be found here: https://woojiahao.notion.site/chirp-2f9f881eda0580158a73f6a072d7453b
+void clear_display(Chirp *chirp);                            // 00E0
+void draw(Chirp *chirp, int vx, int vy, uint8_t n);          // DXYN
+void subroutine_return(Chirp *chirp);                        // 00EE
+void jump(Chirp *chirp, uint8_t nnn);                        // 1NNN
+void subroutine_call(Chirp *chirp, uint8_t nnn);             // 2NNN
+void skip_if_vx_eq_nn(Chirp *chirp, int vx, uint8_t nn);     // 3XNN
+void skip_if_vx_neq_nn(Chirp *chirp, int vx, uint8_t nn);    // 4XNN
+void skip_if_vx_eq_vy(Chirp *chirp, int vx, int vy);         // 5XY0
+void set_vx_eq_nn(Chirp *chirp, int vx, uint8_t nn);         // 6XNN
+void add_nn_to_vx(Chirp *chirp, int vx, uint8_t nn);         // 7XNN
+void set_vx_to_vy(Chirp *chirp, int vx, int vy);             // 8XY0
+void set_vx_eq_vx_or_vy(Chirp *chirp, int vx, int vy);       // 8XY1
+void set_vx_eq_vx_and_vy(Chirp *chirp, int vx, int vy);      // 8XY2
+void set_vx_eq_vx_xor_vy(Chirp *chirp, int vx, int vy);      // 8XY3
+void set_vx_eq_vx_plus_vy(Chirp *chirp, int vx, int vy);     // 8XY4
+void set_vx_eq_vx_minus_vy(Chirp *chirp, int vx, int vy);    // 8XY5
+void set_vx_eq_vx_minus_vy(Chirp *chirp, int vx, int vy);    // 8XY5
+void set_vx_eq_vy_shift_right(Chirp *chirp, int vx, int vy); // 8XY6
+void set_vx_eq_vy_minus_vx(Chirp *chirp, int vx, int vy);    // 8XY7
+void set_vx_eq_vy_shift_left(Chirp *chirp, int vx, int vy);  // 8XYE
+void skip_if_vx_neq_vy(Chirp *chirp, int vx, int vy);        // 9XY0
+void set_index_eq_nnn(Chirp *chirp, uint8_t nnn);            // ANNN
+void jump_with_offset(Chirp *chirp, uint8_t nnn);            // BNNN
+void set_vx_eq_random(Chirp *chirp, int vx, uint8_t nn);     // CXNN
+void skip_if_key_eq_vx(Chirp *chirp, int vx);                // EX9E
+void skip_if_key_neq_vx(Chirp *chirp, int vx);               // EX9E
+void set_vx_eq_delay(Chirp *chirp, int vx);                  // FX07
+void get_key(Chirp *chirp, int vx);                          // FX0A
+void set_delay_eq_vx(Chirp *chirp, int vx);                  // FX15
+void set_sound_eq_vx(Chirp *chirp, int vx);                  // FX18
+void set_I_eq_I_plus_vx(Chirp *chirp, int vx);               // FX1E
+void set_I_eq_vx(Chirp *chirp, int vx);                      // FX29
+void binary_coded_decimal_conversion(Chirp *chirp, int vx);  // FX33
+void set_registers(Chirp *chirp, int vx);                    // FX55
+void load_registers(Chirp *chirp, int vx);                   // FX65
 
 #endif // CHIRP_INSTRUCTIONS_H
